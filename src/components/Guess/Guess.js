0cf7c7ed17c2189040ac54c5b9688c9a7ea7a684
Josue Davila <josuedavila90@gmail.com>
2023-09-17 10:44:14 -0700

    Exercise 3: Refactored GuessResults and Guess components

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { range } from "../../utils";
-import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
-function Guess({ Guesses }) {
+function Guess({ value }) {
   return (
     <>
-      {range(0, NUM_OF_GUESSES_ALLOWED).map((x, i) => (
-        <p key={i} className="guess">
-          {range(0, 5).map((y, j) => (
-            <span key={j} className="cell">
-              {Guesses[i]?.charAt(j)}
-            </span>
-          ))}
-        </p>
-      ))}
+      <p className="guess">
+        {range(5).map((num) => (
+          <span key={num} className="cell">
+            {value?.charAt(num)}
+          </span>
+        ))}
+      </p>
     </>
   );
 }
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Guess from "../Guess";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 function GuessResults({ Guesses }) {
   return (
     <div className="guess-results">
-      <Guess Guesses={Guesses} />
+      {range(0, NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={Guesses[num]} />
+      ))}
     </div>
   );
 }
