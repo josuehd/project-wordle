7849fbdb2ceef6041ec3760107cd44e659ef1170
Josue Davila <josuedavila90@gmail.com>
2023-09-17 00:15:50 -0700

    Exercise 1: adding required to input field, as well as a more meaningful error message when pattern is not followed

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -14,10 +14,12 @@ function GuessInput() {
     >
       <label htmlFor="guess-input">Enter guess:</label>
       <input
+        required
         id="guess-input"
         type="text"
         value={guess}
         pattern="^[A-Z]{5}$"
+        title={"Must be 5 uppercase letters"}
         onChange={(e) => {
           setGuess(e.target.value.toUpperCase());
         }}
